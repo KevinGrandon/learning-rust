ed3062d2788b2e4444a2dacb94d9b2a6330258e9
Kevin Grandon <kevingrandon@yahoo.com>
2014-08-03 12:54:44 -0700

    Hello world.

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Rust experiment.
+
+This is just a placeholder repository for a place to experiment with Rust.
---
+++ examples/01-hello-world.rs
@@ -0,0 +1,6 @@
+use std::io::println;
+
+fn main() {
+	std::io::println("Hello world.");
+	println("Local println");
+}
