f9d94953d2cc6ecb5d9ca3d6b89b4e60573b3cee
Kevin Grandon <kevingrandon@yahoo.com>
2014-08-03 13:37:46 -0700

    Add example #7, if else.

---
+++ examples/07-if-else.rs
@@ -0,0 +1,29 @@
+fn main() {
+    //let n = 5;
+    let mut n;
+    n = 20i;
+
+    if n < 0 {
+        print!("{} is negative", n);
+    } else if n > 0 {
+        print!("{} is positive", n);
+    } else {
+        print!("{} is zero", n);
+    }
+
+    let big_n =
+        if n < 10 && n > -10 {
+            println!(", and is a small number, increase ten-fold");
+
+            // This expression returns an `int`
+            10 * n
+        } else {
+            println!(", and is a big number, reduce by two");
+
+            // This expression must return an `int` as well
+            n / 2
+            // TODO ^ Try suppressing this expression with a semicolon
+        };
+
+    println!("{} -> {}", n, big_n);
+}
