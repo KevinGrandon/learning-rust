9b415a7ded6831bb6add3f5f4642f4f5eb17131b
Kevin Grandon <kevingrandon@yahoo.com>
2014-08-03 12:58:39 -0700

    Add formatted print example

--- examples/01-hello-world.rs
+++ examples/01-hello-world.rs
@@ -1,6 +1,14 @@
-use std::io::println;
+// This is a comment, and will be ignored by the compiler
+// You can test this code by clicking the "Run" button over there ->
+// or if prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
 
+// This code is editable, feel free to hack it!
+// You can always return to the original code by clicking the "Reset" button ->
+
+// This is the main function
 fn main() {
-	std::io::println("Hello world.");
-	println("Local println");
+    // The statements here will be executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
 }
