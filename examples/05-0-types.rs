a7db4ae551653631e3732bf5c7b80216b5db48dd
Kevin Grandon <kevingrandon@yahoo.com>
2014-08-03 13:18:53 -0700

    Add example #5, types.

---
+++ examples/05-0-types.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // Type annotated variable
+    // let a_float: f64 = 1.0;
+    let _a_float: f64 = 1.0;
+
+    // This variable is an `int`
+    // let mut an_integer = 5i;
+    let mut _an_integer = 5i;
+
+    // Error! The type of a variable can't be changed
+    // an_integer = true;
+}
