cff6074a60d6225409615d1e4e17045afcdebf3f
Kevin Grandon <kevingrandon@yahoo.com>
2014-08-04 22:32:32 -0700

    Add example #16.1, match destructuring.

---
+++ examples/16-pattern-matching/16-1-match-destructuring.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // let pair = (2i, -2);
+    // let pair = (2i, 2i);
+    let pair = (3i, 2i);
+    // TODO ^ Try different values for `pair`
+
+    println!("Tell me about {}", pair);
+    // Match can be used to destructure a tuple
+    match pair {
+        // Destructure the tuple
+        (x, y) if x == y => println!("These are twins"),
+        // The ^ `if condition` part is a guard
+        (x, y) if x + y == 0 => println!("Antimatter, kaboom!"),
+        // `_` means don't bind the value to a variable
+        (x, _) if x % 2 == 1 => println!("The first one is odd"),
+        _ => println!("No correlation..."),
+    }
+}
