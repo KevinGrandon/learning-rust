92981c9b6195bd961a93b4dd44878e123eeee9ff
Kevin Grandon <kevingrandon@yahoo.com>
2014-08-05 17:42:59 -0700

    Modify example #26.1.

--- examples/26-enums/26-1-enums-c-like.rs
+++ examples/26-enums/26-1-enums-c-like.rs
@@ -24,14 +24,16 @@ enum Color {
     Red = 0xff0000,
     Green = 0x00ff00,
     Blue = 0x0000ff,
+    Kevin = 0xffffff,
 }
 
 fn main() {
-    let today = Monday;
+    let today = Sunday;
 
     today.mood();
 
     // enums can be casted into integers
     println!("roses are #{:06x}", Red as int);
     println!("violets are #{:06x}", Blue as int);
+    println!("kevin is #{:06x}", Kevin as int);
 }
